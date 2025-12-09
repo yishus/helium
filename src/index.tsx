581eebde6dcadc6a0ab7a658b71302d3ee451f35
Yishu See <yishuyishu@gmail.com>
2025-12-09 16:49:22 +0800

    Update initial screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,44 @@
-import { createCliRenderer, TextAttributes } from "@opentui/core";
+import { createCliRenderer, type SubmitEvent } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 
 function App() {
+  const handleSubmit = (event: SubmitEvent) => {
+    console.log("User submitted:", event);
+  };
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
+    <box
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flexGrow: 1,
+      }}
+    >
+      <box
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          minWidth: 50,
+          width: "50%",
+        }}
+      >
+        <ascii-font font="tiny" text="helium" />
+        <box
+          style={{
+            border: true,
+            width: "100%",
+            flexDirection: "row",
+          }}
+        >
+          <text>&gt; </text>
+          <textarea
+            placeholder="Type here..."
+            onSubmit={handleSubmit}
+            minHeight={1}
+            maxHeight={6}
+            focused
+            flexGrow={1}
+          />
+        </box>
       </box>
     </box>
   );
